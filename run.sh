ed0699c58ac3fe2843dcb2fe8e8a1fb902df9eaf
Samyak Jain <44469244+samyakjain0112@users.noreply.github.com>
2021-10-19 10:23:21 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,7 @@
 
 CUDA_VISIBLE_DEVICES=0 python train_OAAT.py > train_OAAT.txt
 
-CUDA_VISIBLE_DEVICES=0 python validation.py > val_OAAT.txt
 
-CUDA_VISIBLE_DEVICES=0 python eval.py --main_model ./model-cifar-ResNet/OAAT_CIFAR10_1_0.2_0_1_3_0.0005_110.pkl > eval_OAAT.txt
+CUDA_VISIBLE_DEVICES=0 python eval.py --data CIFAR10 --arch WideResNet34 --main_model ./model-cifar-ResNet/OAAT_CIFAR10_1_0.45_1_1_3_0.0003_200.pkl > eval_OAAT.txt
 
 
